735a94a3dc470fe1bf3595bf510cd9bbee5f4885
NaveenRajaLaxmana <naveenrajalaxmana@gmail.com>
2022-06-10 20:29:31 +0530

    added exported in service in manifest xml

--- App.js
+++ App.js
@@ -10,133 +10,38 @@ import React,{useEffect} from 'react';
 
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
+ 
   Text,
-  useColorScheme,
-  View,
-  TouchableOpacity
+  View
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+
 
 import PushNotification  from 'react-native-push-notification';
 import tw from 'twrnc';
 
-const Section = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-          tw`bg-[#191e28]`
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-          // tw `bg-blue`
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
 
 
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  const createChannel = () => {
+  const notifyChannel = () =>{
     PushNotification.createChannel({
-      channelId:"text-channel",
-      channelName:"Test Channel"
+      channelId:"text",
+      channelName:"text-channel"
     })
   }
-
-  const handleNotification = () => {
-    PushNotification.localNotification({
-      channelId:"text-channel",
-      title:"You clicked ",
-      message:"its working"
-    })
-  }
-
-  useEffect(() =>{
-    createChannel()
+  useEffect(() => {
+    notifyChannel()
   })
-
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-            <TouchableOpacity onPress={handleNotification}><Text>Click here for notify</Text></TouchableOpacity>
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView>
+      <View style={tw`h-screen`}>
+        <Text></Text>
+      </View>
     </SafeAreaView>
-  );
+  )
 };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    // fontWeight: '600',
-    fontFamily: 'Inter-SemiBold'
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+
 
 export default App;
